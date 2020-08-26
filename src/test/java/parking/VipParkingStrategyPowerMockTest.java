a0d4976e534f03ce886e2dcf7c3c39129d6c1830
Austin <497133212@qq.com>
2020-08-26 20:12:53 +0800

    [Mocking Exercise2][Austin | Teddy] test : add testCalculateHourlyPrice_givenSunday_thenPriceIsDoubleOfSundayPrice and testCalculateHourlyPrice_givenNotSunday_thenPriceIsDoubleOfNonSundayPrice test case

--- src/test/java/parking/VipParkingStrategyPowerMockTest.java
+++ src/test/java/parking/VipParkingStrategyPowerMockTest.java
@@ -1,9 +1,18 @@
 package parking;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Calendar;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ParkingLot.class})
 public class VipParkingStrategyPowerMockTest {
 
     @Test
@@ -11,15 +20,23 @@ public class VipParkingStrategyPowerMockTest {
 
         /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
         * by using PowerMock to mock static method */
-
+        mockStatic(ParkingLot.class);
+        when(ParkingLot.getBasicHourlyPrice()).thenReturn(25);
+        VipParkingStrategy vipParkingStrategy = new VipParkingStrategy();
+        Integer price = vipParkingStrategy.calculateHourlyPrice();
+        assertEquals(Integer.valueOf(50), price);
     }
 
     @Test
     public void testCalculateHourlyPrice_givenNotSunday_thenPriceIsDoubleOfNonSundayPrice(){
-
         /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
          * by using PowerMock to mock static method */
 
+        mockStatic(ParkingLot.class);
+        when(ParkingLot.getBasicHourlyPrice()).thenReturn(20);
+        VipParkingStrategy vipParkingStrategy = new VipParkingStrategy();
+        Integer price = vipParkingStrategy.calculateHourlyPrice();
+        assertEquals(Integer.valueOf(40), price);
 
     }
 }
